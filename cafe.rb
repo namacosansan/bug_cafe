1eb3fc1a6d135c5f07b1dd1854160586a3ed361e
namacosansan <namacosansan@gmail.com>
2024-05-05 12:14:25 +0900

    order-number、totalの修正

--- cafe.rb
+++ cafe.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 DRINKS = [
   { name: 'コーヒー', price: '300' },
   { name: 'カフェラテ', price: '400' },
@@ -7,28 +5,23 @@ DRINKS = [
   { name: 'エスプレッソ', price: '340' },
   { name: '緑茶', price: '450' }
 ].freeze
-
 FOODS = [
   { name: 'チーズケーキ', price: '470' },
   { name: 'アップルパイ', price: '520' },
   { name: 'ホットサンド', price: '410' }
 ].freeze
-
 def take_order(menus)
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
-  order_number = gets.to_i
+  order_number = gets.to_i - 1
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
-
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
 order1 = take_order(DRINKS)
-
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
-
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
