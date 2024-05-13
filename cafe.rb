b1c95cf07a080a1ec689b983fd3854890dc33afe
namacosansan <namacosansan@gmail.com>
2024-05-13 11:34:22 +0900

    order1、order2の変数名の修正

--- cafe.rb
+++ cafe.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 DRINKS = [
   { name: 'コーヒー', price: '300' },
   { name: 'カフェラテ', price: '400' },
@@ -5,11 +7,13 @@ DRINKS = [
   { name: 'エスプレッソ', price: '340' },
   { name: '緑茶', price: '450' }
 ].freeze
+
 FOODS = [
   { name: 'チーズケーキ', price: '470' },
   { name: 'アップルパイ', price: '520' },
   { name: 'ホットサンド', price: '410' }
 ].freeze
+
 def take_order(menus)
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
@@ -19,9 +23,12 @@ def take_order(menus)
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
+
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drinks = take_order(DRINKS)
+
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+foods = take_order(FOODS)
+
+total = DRINKS[drinks][:price].to_i + FOODS[foods][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
